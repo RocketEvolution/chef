03a660b1cac9e17436f1a47627c0f5a4a00d31f4
Rocket Evolution <rocket.evolution@yahoo.com>
2016-12-20 15:07:50 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -16,20 +16,8 @@ $(document).ready(function() {
 	});
 
 
-	// SLIDER HEIGHT
-	var slideHeight = $(window).height();
-	$('#slider .carousel-inner .item, #slider .video-container').css('height',slideHeight);
-	$(window).resize(function(){'use strict',
-		$('#slider .carousel-inner .item, #slider .video-container').css('height',slideHeight);
-	});
-
-
-	// BANNER HEIGHT
-	var bannerHeight = $(window).height()/1.75;
-	$('#banner .banner-item').css('height',bannerHeight);
-	$(window).resize(function(){'use strict',
-		$('#banner .banner-item').css('height',bannerHeight);
-	});
+	// TESTIMONIALS
+	$("#cslide-slides").cslide();
 
 
 	// BACK TO TOP
@@ -37,12 +25,6 @@ $(document).ready(function() {
 		$('html, body').animate({'scrollTop': '0px'}, 1000);
 		return false;
 	});
-
-
-	// PRETTY PHOTO
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
 
 
 });
